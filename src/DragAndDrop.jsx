9e51ef41ff6a8c152a34e3468c26ec1ef51ee622
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-12-14 21:15:41 +0530

    Drag And drop with bugs

--- src/DragAndDrop.jsx
+++ src/DragAndDrop.jsx
@@ -1,20 +1,81 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const DragAndDrop = ({ data: intialData }) => {
   const [data, setData] = useState(intialData);
 
   const mainHeadings = Object.keys(data); //["Office_Task","Home_Task","Sunday_Task"]
 
+  const dragItem = useRef(); // This will store the information of the source task , idx , container
+  const dragOverItem = useRef(); // This will store the information of the destination   heading , idx
+
+  function handleStartDrag(e, task, heading, idx) {
+    e.target.style.opacity = 0.5;
+    dragItem.current = {
+      task,
+      heading,
+      idx,
+    };
+  }
+
+  function handleDragEnd(e) {
+    e.target.style.opacity = 1;
+  }
+
+  function handleDragEnter(e, idx, heading) {
+    dragOverItem.current = { idx, heading };
+  }
+
+  function handleDragOver(e) {
+    e.preventDefault();
+  }
+
+  function handleDrop() {
+    const source = dragItem.current;
+    const dest = dragOverItem.current;
+
+    if (!source || !dest) return null;
+
+    setData((pre) => {
+      let newSourceList = [...pre[source.heading]];
+      let newDestinationList = [...pre[dest.heading]];
+
+      const [removedItem] = newSourceList.splice(source.idx, 1);
+      newDestinationList.splice(dest.idx, 0, removedItem);
+
+      return {
+        ...pre,
+        [source.heading]: newSourceList,
+        [dest.heading]: newDestinationList,
+      };
+    });
+  }
+
   return (
     <div style={style.root}>
       {mainHeadings.map((heading) => {
         return (
-          <div style={style?.container} key={heading}>
+          <div
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+            style={style?.container}
+            key={heading}
+          >
             <p style={style?.heading}>{heading.replace("_", " ")}</p>
             <div style={style.tasksContainer}>
-              {data[heading].map((task) => {
+              {data[heading].map((task, idx) => {
                 return (
-                  <div draggable style={style.task} key={task.id}>
+                  <div
+                    onDragStart={(e) => {
+                      handleStartDrag(e, task, heading, idx);
+                    }}
+                    onDragEnd={handleDragEnd}
+                    onDragEnter={(e) => {
+                      handleDragEnter(e, idx, heading);
+                    }}
+                    draggable
+                    style={style.task}
+                    key={task.id}
+                  >
                     <p style={style.taskTitle}>{task.title}</p>
                   </div>
                 );
@@ -47,7 +108,7 @@ const style = {
     padding: 10,
     border: "1px solid black",
     width: "80%",
-    backgroundColor: "#90AB8B",
+    backgroundColor: "#EBF4DD",
     borderRadius: 12,
     display: "flex",
     justifyContent: "space-evenly",
@@ -59,9 +120,10 @@ const style = {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    border: "1px solid black",
+
     width: "70%",
-    backgroundColor: "#EBF4DD",
+    backgroundColor: "#90AB8B",
+    color: "white",
     borderRadius: 12,
     marginTop: 10,
   },
